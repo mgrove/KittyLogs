3d693d12746a7d5a35ed816c81f8d1c34aefd495
mgrove <grove266@morris.umn.edu>
2016-10-04 03:11:51 -0500

    Edited manifest file for action bar

--- app/src/main/java/com/mycompany/kittylogs/CatProfileActivity.java
+++ app/src/main/java/com/mycompany/kittylogs/CatProfileActivity.java
@@ -12,6 +12,7 @@ public class CatProfileActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+ //       getSupportActionBar().hide();
         setContentView(R.layout.activity_cat_profile);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
