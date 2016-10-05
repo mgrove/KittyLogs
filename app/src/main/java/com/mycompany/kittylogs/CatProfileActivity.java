4956444b68c72fe555b1ce9ec4df3cf0bc3a5d10
mgrove <grove266@morris.umn.edu>
2016-10-05 17:51:56 -0500

    Refactored CatProfileActivity, commented out the floating action button (not sure if I will use it later)

--- app/src/main/java/com/mycompany/kittylogs/CatProfileActivity.java
+++ app/src/main/java/com/mycompany/kittylogs/CatProfileActivity.java
@@ -18,25 +18,26 @@ public class CatProfileActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
  //       getSupportActionBar().hide();
         setContentView(R.layout.activity_cat_profile);
+        makeNameView();
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+    }
+
+    private void makeNameView(){
         Intent intent = getIntent();
         long catID = intent.getLongExtra(HomeScreen.CLICKED_CAT, 0);
         textView = (TextView)findViewById(R.id.cat_name);
         textView.setTextSize(40);
         DBHelper aHelper = new DBHelper(getApplicationContext());
         textView.setText(aHelper.getCatNameFromDB(catID));
-        ViewGroup layout = (ViewGroup) findViewById(R.id.content_cat_profile);
-//        layout.addView(textView);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
 }
