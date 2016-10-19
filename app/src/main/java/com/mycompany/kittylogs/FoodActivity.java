9c26b59cc708cb680af0bc94ad809e46995d860a
mgrove <grove266@morris.umn.edu>
2016-10-19 02:48:33 -0500

    Added up button and title to FoodActivity

--- app/src/main/java/com/mycompany/kittylogs/FoodActivity.java
+++ app/src/main/java/com/mycompany/kittylogs/FoodActivity.java
@@ -1,5 +1,6 @@
 package com.mycompany.kittylogs;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,14 +9,33 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 public class FoodActivity extends AppCompatActivity {
+    DBHelper aHelper;
+    long catID;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_food);
+        aHelper = new DBHelper(getApplicationContext());
+        catID = getCatID();
+        setActionBar();
+        setFloatingActionButton();
+    }
+
+    private long getCatID(){
+        Intent intent = getIntent();
+        return intent.getLongExtra(CatProfileActivity.CAT_ID,0);
+    }
+
+    public void setActionBar(){
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        setTitle("Food for " + aHelper.getValueFromDB(KittyLogsContract.CatsTable.COLUMN_CAT_NAME, KittyLogsContract.CatsTable.TABLE_NAME, KittyLogsContract.CatsTable._ID, catID));
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
 
+    private void setFloatingActionButton(){
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -24,6 +44,7 @@ public class FoodActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        fab.hide();
     }
 
 }
