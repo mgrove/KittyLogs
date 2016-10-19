3aba49118d19570c28f1e72ee332afa5e136d30e
mgrove <grove266@morris.umn.edu>
2016-10-19 15:49:24 -0500

    Fixed openAddFoodDialog. I'm not actually sure whether I'm going to make the add food view a dialog or another activity.

--- app/src/main/java/com/mycompany/kittylogs/FoodActivity.java
+++ app/src/main/java/com/mycompany/kittylogs/FoodActivity.java
@@ -1,12 +1,17 @@
 package com.mycompany.kittylogs;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.database.DatabaseUtils;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 public class FoodActivity extends AppCompatActivity {
     DBHelper aHelper;
@@ -47,6 +52,32 @@ public class FoodActivity extends AppCompatActivity {
         fab.hide();
     }
 
-    public void openAddFoodDialog(){}
+    public void openAddFoodDialog(View view){
+//        AlertDialog.Builder addDialogBuilder = new AlertDialog.Builder(this);
+//        addDialogBuilder.setTitle(R.string.new_note_dialog_title);
+//        final EditText input = new EditText(this);
+//        addDialogBuilder.setView(input);
+//        setAddButtons(addDialogBuilder, input);
+//        AlertDialog addDialog = addDialogBuilder.create();
+//        addDialog.show();
+    }
+
+//    private void setAddButtons(AlertDialog.Builder builder, final EditText input){
+//        builder.setPositiveButton("Add note", new DialogInterface.OnClickListener() {
+//            public void onClick(DialogInterface dialog, int id) {
+//    //            String value = input.getText().toString();
+//                Log.d("Table name", KittyLogsContract.NotesTable.TABLE_NAME);
+//   //             aHelper.addEntryToDB(makeNoteContentValues(value), KittyLogsContract.NotesTable.TABLE_NAME);
+//                Log.d("Notes Table", DatabaseUtils.dumpCursorToString(aHelper.getTableCursorFromDB(KittyLogsContract.NotesTable.TABLE_NAME)));
+//  //              loadDataWithCursor();
+//                return;
+//            }
+//        });
+//        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+//            public void onClick(DialogInterface dialog, int id) {
+//                return;
+//            }
+//        });
+//    }
 
 }
