054d587c4851b3833678808bdfbc49b3b187f6a2
mgrove <grove266@morris.umn.edu>
2016-10-24 04:02:39 -0500

    Added tabs to WeightActivity (one will have the chart view and the other will have the list view)

--- app/src/main/java/com/mycompany/kittylogs/WeightActivity.java
+++ app/src/main/java/com/mycompany/kittylogs/WeightActivity.java
@@ -20,6 +20,7 @@ import android.widget.AdapterView;
 import android.widget.CursorAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TabHost;
 import android.widget.TableLayout;
 
 import static java.lang.System.currentTimeMillis;
@@ -44,6 +45,22 @@ public class WeightActivity extends AppCompatActivity {
         listView = (ListView) findViewById(R.id.weight_list);
         registerForContextMenu(listView);
         loadDataWithCursor();
+
+        TabHost host = (TabHost)findViewById(R.id.tab_host);
+        host.setup();
+
+        //Tab 1
+        TabHost.TabSpec spec = host.newTabSpec("Chart View");
+        spec.setContent(R.id.tab1);
+        spec.setIndicator("Chart View");
+        host.addTab(spec);
+
+        //Tab 2
+        spec = host.newTabSpec("List View");
+        spec.setContent(R.id.tab2);
+        spec.setIndicator("List View");
+        host.addTab(spec);
+
     }
 
     private long getCatID() {
