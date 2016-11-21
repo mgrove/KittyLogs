2aae3630c08931a7088e471e754588442a5f7154
mgrove <grove266@morris.umn.edu>
2016-11-20 18:37:13 -0600

    Added Mockito to Gradle

---
+++ app/src/test/java/com/mycompany/kittylogs/HomeScreenTest.java
@@ -0,0 +1,50 @@
+package com.mycompany.kittylogs;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by System User on 11/14/2016.
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class HomeScreenTest {
+    @Test
+    public void onCreate() throws Exception {
+
+    }
+
+    @Test
+    public void makeCatContentValues() throws Exception {
+
+    }
+
+    @Test
+    public void onCreateContextMenu() throws Exception {
+
+    }
+
+    @Test
+    public void onContextItemSelected() throws Exception {
+
+    }
+
+    @Test
+    public void onItemClick() throws Exception {
+
+    }
+
+    @Test
+    public void onCreateOptionsMenu() throws Exception {
+
+    }
+
+    @Test
+    public void onOptionsItemSelected() throws Exception {
+
+    }
+
+}
