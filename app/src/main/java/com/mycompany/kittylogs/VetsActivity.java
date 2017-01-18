0a9e6a262c49781b1b546073d1a32a327a65cae7
mgrove <grove266@morris.umn.edu>
2017-01-17 20:30:51 -0600

    Finished VetsCursorAdapter

--- app/src/main/java/com/mycompany/kittylogs/VetsActivity.java
+++ app/src/main/java/com/mycompany/kittylogs/VetsActivity.java
@@ -13,6 +13,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
 
 public class VetsActivity extends AppCompatActivity {
 
@@ -40,10 +41,34 @@ public class VetsActivity extends AppCompatActivity {
             cursorInflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         }
 
-        public void bindView(View view, Context context, Cursor cursor){}
+        public String convertBoolIntToString(int input){
+            if (input==1){
+                return "Yes";
+            } else {
+                return "No";
+            }
+        }
+
+        public void bindView(View view, Context context, Cursor cursor){
+            TextView nameTextView = (TextView) view.findViewById(R.id.vet_name);
+            TextView phoneTextView = (TextView) view.findViewById(R.id.vet_phone);
+            TextView addressTextView = (TextView) view.findViewById(R.id.vet_address);
+            TextView websiteTextView = (TextView) view.findViewById(R.id.vet_website);
+            TextView emergencyTextView = (TextView) view.findViewById(R.id.is_emergency_vet);
+            String name = cursor.getString(cursor.getColumnIndex(KittyLogsContract.VetsTable.COLUMN_VET_NAME));
+            String phone = cursor.getString(cursor.getColumnIndex(KittyLogsContract.VetsTable.COLUMN_PHONE));
+            String address = cursor.getString(cursor.getColumnIndex(KittyLogsContract.VetsTable.COLUMN_ADDRESS));
+            String website = cursor.getString(cursor.getColumnIndex(KittyLogsContract.VetsTable.COLUMN_WEBSITE));
+            String emergency = "Emergency vet? " + convertBoolIntToString(cursor.getInt(cursor.getColumnIndex(KittyLogsContract.VetsTable.COLUMN_EMERGENCY)));
+            nameTextView.setText(name);
+            phoneTextView.setText(phone);
+            addressTextView.setText(address);
+            websiteTextView.setText(website);
+            emergencyTextView.setText(emergency);
+        }
 
         public View newView(Context context, Cursor cursor, ViewGroup parent){
-            return cursorInflater.from(context).inflate(R.layout.cat_list_text_view, parent, false);
+            return cursorInflater.from(context).inflate(R.layout.vet_list_text_view, parent, false);
         }
 
     }
