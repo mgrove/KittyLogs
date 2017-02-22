4c05afcf6ebd1d0185e6c4e786e300af41a46e08
mgrove <grove266@morris.umn.edu>
2017-02-22 01:37:01 -0600

    Added diagnoses list, date to visit profile

--- app/src/main/java/com/mycompany/kittylogs/VisitProfileActivity.java
+++ app/src/main/java/com/mycompany/kittylogs/VisitProfileActivity.java
@@ -23,13 +23,18 @@ public class VisitProfileActivity extends AppCompatActivity {
     DBHelper aHelper;
     private Cursor vaccinesCursor;
     private VaccinesCursorAdapter vaccinesCursorAdapter;
+    private Cursor diagnosesCursor;
+    private DiagnosesCursorAdapter diagnosesCursorAdapter;
 
     long catID;
     long visitID;
     String catName;
     String vetName;
+    String date;
     TextView vetNameTextView;
+    TextView dateTextView;
     ListView vaccinesListView;
+    ListView diagnosesListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,24 +42,35 @@ public class VisitProfileActivity extends AppCompatActivity {
         setContentView(R.layout.activity_visit_profile);
         setVariables();
         setActionBar();
-        setVetTextView();
+        setVetAndDateTextView();
         loadVaccineDataWithCursor();
+        loadDiagnosisDataWithCursor();
     }
 
     private void setVariables(){
         vaccinesListView = (ListView) findViewById(R.id.visit_vaccine_list);
+        diagnosesListView = (ListView) findViewById(R.id.visit_diagnosis_list);
         aHelper = new DBHelper(getApplicationContext());
         setCatAndVet();
         Log.d("Vet ID:", Long.toString(visitID));
         catName = aHelper.getValueFromDB(KittyLogsContract.CatsTable.COLUMN_CAT_NAME, KittyLogsContract.CatsTable.TABLE_NAME, KittyLogsContract.CatsTable._ID, catID);
         Long vetID = Long.parseLong(aHelper.getValueFromDB(KittyLogsContract.VetVisitsTable.COLUMN_VET_IDFK, KittyLogsContract.VetVisitsTable.TABLE_NAME, KittyLogsContract.VetVisitsTable._ID, visitID));
         vetName = aHelper.getValueFromDB(KittyLogsContract.VetsTable.COLUMN_VET_NAME, KittyLogsContract.VetsTable.TABLE_NAME, KittyLogsContract.VetsTable._ID, vetID);
+        String dateInMilliseconds = aHelper.getValueFromDB(KittyLogsContract.VetVisitsTable.COLUMN_DATE,
+                KittyLogsContract.VetVisitsTable.TABLE_NAME,
+                KittyLogsContract.VetVisitsTable._ID,
+                visitID);
+        date = Extras.convertMillisecondsToDate(Long.parseLong(dateInMilliseconds));
     }
 
-    private void setVetTextView(){
-            vetNameTextView = (TextView)findViewById(R.id.visit_profile_vet_name);
-            vetNameTextView.setTextSize(40);
-            vetNameTextView.setText(vetName);
+    private void setVetAndDateTextView(){
+        dateTextView = (TextView) findViewById(R.id.visit_profile_date);
+        dateTextView.setTextSize(40);
+        dateTextView.setText(date);
+
+        vetNameTextView = (TextView)findViewById(R.id.visit_profile_vet_name);
+        vetNameTextView.setTextSize(30);
+        vetNameTextView.setText(vetName);
 
     }
 
@@ -111,6 +127,47 @@ public class VisitProfileActivity extends AppCompatActivity {
         aHelper.close();
     }
 
+    public void openAddDiagnosisDialog(View view){
+        AlertDialog.Builder addDialogBuilder = new AlertDialog.Builder(this);
+        addDialogBuilder.setTitle("New Diagnosis");
+        final EditText input = new EditText(this);
+        addDialogBuilder.setView(input);
+        setAddDiagnosisButtons(addDialogBuilder, input);
+        AlertDialog addDialog = addDialogBuilder.create();
+        addDialog.show();
+    }
+
+    private void setAddDiagnosisButtons(AlertDialog.Builder builder, final EditText input){
+        builder.setPositiveButton("Add note", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                String value = input.getText().toString();
+                aHelper.addEntryToDB(makeDiagnosisContentValues(value), KittyLogsContract.DiagnosesTable.TABLE_NAME);
+                Log.d("Diagnoses Table", DatabaseUtils.dumpCursorToString(aHelper.getTableCursorFromDB(KittyLogsContract.DiagnosesTable.TABLE_NAME)));
+                loadDiagnosisDataWithCursor();
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+            }
+        });
+    }
+
+    private ContentValues makeDiagnosisContentValues(String entry){
+        ContentValues values = new ContentValues();
+        values.put(KittyLogsContract.DiagnosesTable.COLUMN_DIAGNOSIS, entry);
+        values.put(KittyLogsContract.DiagnosesTable.COLUMN_CAT_IDFK, catID);
+        values.put(KittyLogsContract.DiagnosesTable.COLUMN_VET_VISIT_IDFK, visitID);
+        return values;
+    }
+
+    protected void loadDiagnosisDataWithCursor(){
+        diagnosesCursor = aHelper.getTableCursorForCatAndVisitFromDB(KittyLogsContract.DiagnosesTable.TABLE_NAME, KittyLogsContract.DiagnosesTable.COLUMN_CAT_IDFK, catID, KittyLogsContract.DiagnosesTable.COLUMN_VET_VISIT_IDFK, visitID);
+        diagnosesCursorAdapter = new VisitProfileActivity.DiagnosesCursorAdapter(this, diagnosesCursor, CursorAdapter.FLAG_REGISTER_CONTENT_OBSERVER);
+        diagnosesListView.setAdapter(diagnosesCursorAdapter);
+        aHelper.close();
+    }
+
+
     public class VaccinesCursorAdapter extends CursorAdapter {
 
         private LayoutInflater cursorInflater;
@@ -126,6 +183,29 @@ public class VisitProfileActivity extends AppCompatActivity {
             TextView vaccineTextView = (TextView) view.findViewById(R.id.small_basic_row_view);
             String name = cursor.getString(cursor.getColumnIndex(KittyLogsContract.VaccinesTable.COLUMN_VACCINE_NAME));
             vaccineTextView.setText(name);
+        }
+
+        public View newView(Context context, Cursor cursor, ViewGroup parent){
+            return cursorInflater.from(context).inflate(R.layout.small_basic_row_view, parent, false);
+        }
+
+    }
+
+    public class DiagnosesCursorAdapter extends CursorAdapter {
+
+        private LayoutInflater cursorInflater;
+        private final Context context;
+
+        protected DiagnosesCursorAdapter(Context context, Cursor cursor, int flags){
+            super(context, cursor, flags);
+            this.context = context;
+            cursorInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        }
+
+        public void bindView(View view, Context context, Cursor cursor){
+            TextView diagnosisTextView = (TextView) view.findViewById(R.id.small_basic_row_view);
+            String name = cursor.getString(cursor.getColumnIndex(KittyLogsContract.DiagnosesTable.COLUMN_DIAGNOSIS));
+            diagnosisTextView.setText(name);
         }
 
         public View newView(Context context, Cursor cursor, ViewGroup parent){
